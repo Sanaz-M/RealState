e33944ffb9ec4a7ac240868ba3cf1961d02bc555
Sanaz_M <sanazmohammaddi@gmail.com>
2024-08-13 15:40:36 +0100

    Implementing the Register and Login process

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
 import authRoute from './routes/auth.route';
 import postRoute from './routes/post.route';
 
 const app = express();
 
+app.use(cors({origin: 'http://localhost:5173', credentials:true}))
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
